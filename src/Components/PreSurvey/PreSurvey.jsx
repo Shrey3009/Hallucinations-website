f36c19020d036d6e2c76d93bac602c5683ae442e
Banudeep <60642712+Banudeep@users.noreply.github.com>
2025-03-06 17:55:09 -0500

    Update PreSurvey.jsx

--- src/Components/PreSurvey/PreSurvey.jsx
+++ src/Components/PreSurvey/PreSurvey.jsx
@@ -28,7 +28,7 @@ function SurveyForm() {
     e.preventDefault();
     console.log("Trying to submit Form:", formData);
     const NODE_api = import.meta.env.VITE_SOME_KEY;
-    // console.log("VITE_NODE_URI", NODE_api);
+    console.log("VITE_NODE_URI", NODE_api + "/PreSurvey");
 
     try {
       const response = await fetch(NODE_api + "/PreSurvey", {
